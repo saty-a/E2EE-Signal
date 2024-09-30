54d1e3b5eb978c5cfc5be84e9c832f30d0fc5360
Satya Prakash <satyaprakash6945@gmail.com>
2024-09-30 17:09:37 +0530

    Add: logs

---
+++ lib/log.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Log extends StatelessWidget {
+  const Log({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 100,
+        width: 100,
+        decoration: const BoxDecoration(color: Colors.grey),
+        child: const SizedBox(),
+      ),
+    );
+  }
+}
